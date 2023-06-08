0bae3ba2d571dcf391c1a9813e343af94f52a3c3
Xanonymous <trusaidlin@gmail.com>
2023-06-09 05:56:55 +0800

    feat: 🎸 add AutoCheckRecognizableStrEnum

---
+++ utils/auto_recognizable_str_enum.py
@@ -0,0 +1,26 @@
+from enum import StrEnum
+
+__all__ = ['AutoCheckRecognizableStrEnum']
+
+
+class AutoCheckRecognizableStrEnum(StrEnum):
+    @classmethod
+    def _missing_(cls, key):
+        """
+        This method is invoked when an invalid enum value is passed.
+        It raises a ValueError with a message that lists all the valid enum values.
+        """
+        raise ValueError(f'Invalid {cls.__name__} value: {key}. Valid values are: {[e.value for e in cls]}')
+
+    @classmethod
+    def is_valid(cls, value):
+        """
+        This method checks if the provided value is a valid enum value.
+        """
+        return value in cls._value2member_map_
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.value
