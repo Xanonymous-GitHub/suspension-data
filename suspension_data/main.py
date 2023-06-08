f01b54c1609bae49015a319f399a8b1baba5a195
Xanonymous <trusaidlin@gmail.com>
2023-06-09 05:59:01 +0800

    feat: 🎸 add some test code for trying the dto

--- suspension_data/main.py
+++ suspension_data/main.py
@@ -1,6 +1,25 @@
+import os
+from typing import Final
+
+from suspension_data.constants import DATA_SOURCE_LOCATION
+from suspension_data.dto import SuspensionCsvDto
+from suspension_data.models import SuspensionRecord
+
+
+def read_csv_content():
+    dto: Final[SuspensionCsvDto] = SuspensionCsvDto(f'{DATA_SOURCE_LOCATION}/university_suspension_data.csv')
+    records: Final[tuple[SuspensionRecord]] = dto.to_suspension_records()
+
+    # pretty print the records
+    for record in records:
+        print(record.serializable_dict)
+
+
 def start():
-    pass
+    read_csv_content()
 
 
 if __name__ == '__main__':
+    # This is needed to run the script from the root directory
+    os.chdir('..')
     start()
